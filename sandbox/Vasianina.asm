396003416320ba1ffefdac6ec5686616472d3c4c
vasianinansts <149393904+vasianinansts@users.noreply.github.com>
2026-09-16 17:45:35 +0000

    start

--- sandbox/Vasianina.asm
+++ sandbox/Vasianina.asm
@@ -1,42 +1,34 @@
 format ELF
 public _start
 
-section '.text' executable
-
-_start:
-    ; 1. Вывод Фамилии
-    mov eax, 4          ; sys_write (4)
-    mov ebx, 1          ; stdout (1)
-    mov ecx, last_name  ; адрес строки
-    mov edx, len_last   ; длина строки
-    int 0x80
-
-    ; 2. Вывод Имени
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, first_name
-    mov edx, len_first
-    int 0x80
-
-    ; 3. Вывод Отчества
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, patronymic
-    mov edx, len_patr
-    int 0x80
-
-    ; 4. Завершение программы (sys_exit)
-    mov eax, 1          ; sys_exit (1)
-    xor ebx, ebx        ; код возврата 0
-    int 0x80
-
 section '.data' writeable
+    fam db 'Vasianina', 10
+    fam_len = $ - fam
+    name db 'Anastasia', 10
+    name_len = $ - name
+    pat db 'Alecsandrovna', 10
+    pat_len = $ - pat
 
-last_name   db "Vasianina", 0xA
-len_last    = $ - last_name
+section '.text' executable
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, fam
+    mov edx, fam_len
+    int 0x80
 
-first_name  db "Anastasia", 0xA
-len_first   = $ - first_name
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, name
+    mov edx, name_len
+    int 0x80
 
-patronymic  db "Alecsandrovna", 0xA
-len_patr    = $ - patronymic
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, pat
+    mov edx, pat_len
+    int 0x80
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
